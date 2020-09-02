e544f0c8d18363865d032d1b29ef6d03583e0b1f
Jake West <jacobkwest93@gmail.com>
2020-09-02 11:41:18 -0600

    Add integration test for App for adding new song

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,7 +1,28 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('true should be true', () => {
-  expect(true).toEqual(true);
-});
+describe('App', () => {
+  it('should add a new song when the form is submitted', () => {
+    render(<App />)
+
+    const songInput = screen.getByPlaceholderText('Song Name')
+    const artistInput = screen.getByPlaceholderText('Artist Name')
+    const linkInput = screen.getByPlaceholderText('Link')
+
+    fireEvent.change(songInput, { target: { value: 'Vibe (If I Back it Up)'} })
+    fireEvent.change(artistInput, { target: { value: 'Cookiee Kawaii'} })
+    fireEvent.change(linkInput, { target: { value: 'https://www.youtube.com/watch?v=Pf4Be8xeD9c'} })
+
+    const submitButton = screen.getByText('Submit')
+    fireEvent.click(submitButton)
+
+    const songName = screen.getByText('Vibe (If I Back it Up)')
+    const artistName = screen.getByText('Cookiee Kawaii')
+    const link = screen.getByText('Watch video!')
+
+    expect(songName).toBeInTheDocument()
+    expect(artistName).toBeInTheDocument()
+    expect(link).toBeInTheDocument()
+  })
+})
